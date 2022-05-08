15a1c6490bc8cecc578bace76d9178014f68bfee
leadgeex <aaronleadgeex@gmail.com>
2022-05-08 23:59:29 +0800

    added a submit button

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -62,16 +62,16 @@ const Contact = () => {
                     <div className="relative z-0 w-full mb-6 group">
                         <input
                             type="text"
-                            name="floating_company"
+                            name="floating_phone"
                             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-pink-500 focus:outline-none focus:ring-0 focus:border-pink-600 peer"
                             placeholder=" "
                             required
                         />
                         <label
-                            htmlFor="floating_company"
+                            htmlFor="floating_phone"
                             className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-pink-600 peer-focus:dark:text-pink-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                         >
-                            Company
+                            Phone
                         </label>
                     </div>
                     <div className="relative z-0 w-full mb-6 group">
@@ -87,6 +87,14 @@ const Contact = () => {
                         >
                             Message
                         </label>
+                    </div>
+                    <div className="text-center">
+                        <button
+                            type="submit"
+                            className="text-white bg-pink-700 hover:bg-pink-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800"
+                        >
+                            Submit
+                        </button>
                     </div>
                 </form>
             </div>
